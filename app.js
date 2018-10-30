d262f967bd5170f2d3b62ba32b2e8e0d300d3819
Chungzilla <chung.jazmine@gmail.com>
2018-10-29 22:36:30 -0400

    hide old images when clicking on new page

--- app.js
+++ app.js
@@ -1,2 +1,49 @@
 // alert("I'm working!!!")
 
+let imagesOnPage = new Array();
+let currentPage = 1;
+let imagesPerPage = 10;
+const events = 
+Array.prototype.slice.call(document.querySelectorAll(".images"));
+
+// console.log(events);
+
+const totalPages = () => Math.ceil(events.length / imagesPerPage);
+
+// console.log(totalPages.length)
+
+let previousPage = () => {currentPage -= 1; createPage()}
+let nextPage = () => {currentPage += 1; createPage()}
+
+
+
+//Dispaly list on page
+const createPage = () => {
+    let firstImage = ((currentPage - 1)) * imagesPerPage;
+    let lastImage = firstImage + imagesPerPage;
+    for (i = 0; i < imagesOnPage.length; i++){
+        imagesOnPage[i].classList.add("hidden")
+            //Hides other images
+    }
+    imagesOnPage = events.slice(firstImage, lastImage);
+    showImages();
+    // check();
+}
+
+const showImages = () => {
+    for (i = 0; i < imagesOnPage.length; i++){
+        imagesOnPage[i].classList.remove("hidden")
+    }
+}
+
+// function check() {
+//     document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
+//     document.getElementById("previous").disabled = currentPage == 1 ? true : false;
+//     document.getElementById("first").disabled = currentPage == 1 ? true : false;
+//     document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
+//   }
+
+const start = () => {createPage()};
+
+// let allPages = totalPages; 
+window.onload = start;
