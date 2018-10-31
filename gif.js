2cd24149e0e5a3ee6c4f78ca64590d8fc40745b9
Chungzilla <chung.jazmine@gmail.com>
2018-10-31 19:11:34 -0400

    added hidden properties

--- gif.js
+++ gif.js
@@ -6,7 +6,7 @@ class Gif{
     }
 }
 
-let showGif = () => {
+let showGifs = () => {
     //Creates request
     let xhr = new XMLHttpRequest();
     console.log(xhr)
@@ -14,6 +14,18 @@ let showGif = () => {
     xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=kyQhfV4Bysw7t6wFTbgjOVEusLvwmGjV&limit=50', true);
 
     console.log(xhr.status);
+
+// readyState Values
+    // 0: request not initialized
+    // 1: server connection established
+    // 2: request received
+    // 3: processing request
+    // 4: request finished and response is ready
+
+//HTTP Statuses
+    // 200 = OK 
+    // 400 = Forbidden
+    // 403 Not Found
 
     xhr.onload = () => {
         console.log('READYSTATE: ', xhr.readyState);
@@ -31,12 +43,29 @@ let showGif = () => {
                 let movingGif = gif.images.original.url; 
                 console.log(movingGif);
                 
-                //This will dynamically create elements need to render the images
+                //This will dynamically create elements needed to render the images
 
                 let gifGallery = document.getElementById('gallery')
                 let displayTitle = document.getElementById('title')
-                let displayStill = document.createElement('img')
                 let displayGif = document.createElement('img')
+                let displayStill = document.createElement('img')
+                //Adding class to hide images by default
+                displayStill.classList.add("images", "hidden")
+
+                //Displays only the first 10 images
+              
+                // for (i = 1; i < 51; i++) {
+                //     displayStill.id = i;
+                // }
+                // // let i = 1
+                // // do {
+                // //     displayStill.id = i;
+                // //     i++;
+                // // }
+                // // while (i < 51);
+
+
+
 
                 let gifWrap = document.createElement('div')
                 gifWrap.id = 'gifWrap'
@@ -80,71 +109,13 @@ let showGif = () => {
 
 
 }
+const start = () => showGifs();
 
-
-
-document.getElementById('button').addEventListener('click', showGif)
-
-// readyState Values
-// 0: request not initialized
-// 1: server connection established
-// 2: request received
-// 3: processing request
-// 4: request finished and response is ready
-
-//HTTP Statuses
-// 200 = OK 
-// 400 = Forbidden
-// 403 Not Found
+window.onload = start;
 
 
 
 
 
 
-// let imagesOnPage = new Array();
-// let currentPage = 1;
-// let imagesPerPage = 10;
-// const events = 
-// Array.prototype.slice.call(document.querySelectorAll(".images"));
 
-// // console.log(events);
-
-// const totalPages = () => Math.ceil(events.length / imagesPerPage);
-
-// // console.log(totalPages.length)
-
-// let previousPage = () => {currentPage -= 1; createPage()}
-// let nextPage = () => {currentPage += 1; createPage()}
-
-
-
-// //Dispaly list on page
-// const createPage = () => {
-//     let firstImage = ((currentPage - 1)) * imagesPerPage;
-//     let lastImage = firstImage + imagesPerPage;
-//     for (i = 0; i < imagesOnPage.length; i++){
-//         imagesOnPage[i].classList.add("hidden")
-//             //Hides other images
-//     }
-//     imagesOnPage = events.slice(firstImage, lastImage);
-//     showImages();
-//     check();
-// }
-
-// const showImages = () => {
-//     for (i = 0; i < imagesOnPage.length; i++){
-//         imagesOnPage[i].classList.remove("hidden")
-//     }
-// }
-
-// const check = () => {
-//     document.getElementById("next").disabled = currentPage == numOfPages ? true : false;
-//     document.getElementById("previous").disabled = currentPage == 1 ? true : false;
-
-//   }
-
-// const start = () => createPage();
-
-// let numOfPages = totalPages(); 
-// window.onload = start;
