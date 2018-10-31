0d78ba51859ede7317d8244a2740a4df36d07483
Chungzilla <chung.jazmine@gmail.com>
2018-10-31 00:15:19 -0400

    separated gif object

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
 
 
-axios.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=9CIHM7uD1nGkFdHLsNqeGUsOZi4JZUJk&limit=50").then((response) => {
-    console.log(response.data);
-    let data = response.data
-}
-);
-
 let loadText = () => {
+    //Creates request
     let xhr = new XMLHttpRequest();
-    console.log(xhr)
+    // console.log(xhr)
     //open - type, url, async?
     xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=9CIHM7uD1nGkFdHLsNqeGUsOZi4JZUJk&limit=50', true);
 
+    // console.log(xhr.response);
+
+
+
     xhr.onload = () => {
+        console.log('READYSTATE: ', xhr.readyState);
         if (this.status == 200){
-            console.log(tis.response);
+            // console.log(this.response);
+            let gifs = JSON.parse(this.responseText);
+            console.log(gifs);
         }
-    }  
-        xhr.send(); 
+    }
+ 
+
+    xhr.error = () => {
+        console.log('Request Error..');
+    }
+
+
+    // xhr.onreadystatechange = () => {
+    //     console.log('READYSTATE: ', xhr.readyState)
+    //     if (this.readyState == 4 && this.status == 200){
+    //         // console.log(this.responseText);
+    //     }
+    // }
+
+    //Sending Giphy response to browser
+    xhr.send(); 
+
+
 }
 
+document.getElementById('button').addEventListener('click', loadText)
 
+// readyState Values
+// 0: request not initialized
+// 1: server connection established
+// 2: request received
+// 3: processing request
+// 4: request finished and response is ready
+
+//HTTP Statuses
+// 200 = OK 
+// 400 = Forbidden
+// 403 Not Found
 
 let imagesOnPage = new Array();
 let currentPage = 1;
